79bd0ce3967831d05b27467832a905219c8a1797
henryzheng <603702817@qq.com>
2016-12-13 09:59:31 +0800

    M:增加点击标题的监听（注解的缺陷处理）

--- app/src/main/java/com/example/henryzheng/ccimageshare/V/NavigationFragment.java
+++ app/src/main/java/com/example/henryzheng/ccimageshare/V/NavigationFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.os.Message;
 import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -14,7 +13,6 @@ import com.example.henryzheng.ccimageshare.C.Base.BaseFragment;
 import com.example.henryzheng.ccimageshare.R;
 
 import org.xutils.view.annotation.ContentView;
-import org.xutils.view.annotation.Event;
 import org.xutils.view.annotation.ViewInject;
 
 /**
@@ -28,15 +26,16 @@ public class NavigationFragment extends BaseFragment {
     LinearLayout lin0;// title的layout
     int titleWidth = 0;
     float instanceX = 0;
-    private ViewPager viewPager;
-    @ViewInject(R.id.tv0)
+    public  ViewPager viewPager;
+//    @ViewInject(R.id.tv0)
     TextView tv0;
-    @ViewInject(R.id.tv1)
+//    @ViewInject(R.id.tv1)
     TextView tv1;
-    @ViewInject(R.id.tv2)
+//    @ViewInject(R.id.tv2)
     TextView tv2;
 
-    float mPositionOffset=0;
+    float mPositionOffset = 0;
+
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -46,7 +45,8 @@ public class NavigationFragment extends BaseFragment {
         height = lin0.getMeasuredHeight();
         width = lin0.getMeasuredWidth();
         titleWidth = width / 3;
-        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) lin.getLayoutParams();
+        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) lin
+                .getLayoutParams();
         layoutParams.width = titleWidth;
         lin.setLayoutParams(layoutParams);
         final int[] location = new int[2];
@@ -67,30 +67,31 @@ public class NavigationFragment extends BaseFragment {
      *
      * @param viewPager
      */
-    public void setMainPage(ViewPager viewPager) {
+    public void setMainPage(final ViewPager viewPager) {
+
         this.viewPager = viewPager;
         viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            int position = 0;
-            Boolean isIni=false;
+            Boolean isIni = false;
             LinearLayout bottomLin;
             RelativeLayout.LayoutParams linLayoutParams;
-            TextView tv0;
-
             @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            public void onPageScrolled(int position, float positionOffset, int
+                    positionOffsetPixels) {
                 /**
                  * 第一次会默认加载，此时这个fragment还没完全绘制好，故做初始化判断屏蔽
                  */
-                if (!isIni){
-                    isIni=true;
+                if (!isIni) {
+                    isIni = true;
+                   setTitleListener();
                     return;
-                }else
-                {
+                } else {
+                    /**
+                     * 在其他fragment里面操作不能用注解
+                     */
                     bottomLin = (LinearLayout) getActivity().findViewById(R.id.lin5);
                     linLayoutParams = (RelativeLayout.LayoutParams) lin.getLayoutParams();
-                    tv0= (TextView) getActivity().findViewById(R.id.tv0);
                 }
-                mPositionOffset=positionOffset;
+                mPositionOffset = positionOffset;
                 switch (position) {
                     case 0:
                         linLayoutParams.leftMargin = (int) (titleWidth * positionOffset);
@@ -99,12 +100,14 @@ public class NavigationFragment extends BaseFragment {
 
                         break;
                     case 1:
-                        linLayoutParams.leftMargin = titleWidth + (int) (titleWidth * positionOffset);
+                        linLayoutParams.leftMargin = titleWidth + (int) (titleWidth *
+                                positionOffset);
                         bottomLin.setLayoutParams(linLayoutParams);
                         bottomLin.requestLayout();
                         break;
                     case 2:
-                        linLayoutParams.leftMargin = titleWidth * 2 + (int) (titleWidth * positionOffset);
+                        linLayoutParams.leftMargin = titleWidth * 2 + (int) (titleWidth *
+                                positionOffset);
                         bottomLin.setLayoutParams(linLayoutParams);
                         bottomLin.requestLayout();
 
@@ -124,6 +127,36 @@ public class NavigationFragment extends BaseFragment {
             }
         });
 
+
+    }
+
+    /**
+     * 在其他fragment里面操作不能用注解
+     */
+    private void setTitleListener() {
+        tv0= (TextView) getActivity().findViewById(R.id.tv0);
+        tv1= (TextView) getActivity().findViewById(R.id.tv1);
+        tv2= (TextView) getActivity().findViewById(R.id.tv2);
+        tv0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(0);
+            }
+        });
+        tv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(1);
+
+            }
+        });
+        tv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(2);
+
+            }
+        });
     }
 
 
@@ -132,4 +165,6 @@ public class NavigationFragment extends BaseFragment {
         super.OnHandlerListener(msg);
 
     }
+
+
 }
