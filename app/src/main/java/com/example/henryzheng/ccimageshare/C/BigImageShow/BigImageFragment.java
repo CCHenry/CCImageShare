da37cd4ddce8d729c368964ca89a68cbfdbb9b38
henryzheng <603702817@qq.com>
2016-12-27 09:13:41 +0800

    M:优化框架

---
+++ app/src/main/java/com/example/henryzheng/ccimageshare/C/BigImageShow/BigImageFragment.java
@@ -0,0 +1,160 @@
+package com.example.henryzheng.ccimageshare.C.BigImageShow;
+
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.henryzheng.ccimageshare.C.Base.BaseActivity;
+import com.example.henryzheng.ccimageshare.C.Base.BaseFragment;
+import com.example.henryzheng.ccimageshare.M.ZuiMeiModel.Image;
+import com.example.henryzheng.ccimageshare.M.utils.CCLog;
+import com.example.henryzheng.ccimageshare.M.utils.StringUtil;
+import com.example.henryzheng.ccimageshare.R;
+import com.example.henryzheng.ccimageshare.test.MyHorizonScrollView;
+
+import org.xutils.common.Callback;
+import org.xutils.image.ImageOptions;
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+import org.xutils.x;
+
+
+@ContentView(R.layout.fragment_big_image)
+public class BigImageFragment extends BaseFragment {
+    // TODO: Rename parameter arguments, choose names that match
+    Image bigImage = new Image();
+    @ViewInject(R.id.iv)
+    private ImageView iv;
+    @ViewInject(R.id.load)
+    private ImageView load;
+    @ViewInject(R.id.tv0)
+    private TextView tv0;
+    @ViewInject(R.id.horizontalScrollView)
+    private MyHorizonScrollView horizontalScrollView;
+    private ImageOptions imageOptions;
+
+    public void setBigImage(Image bigImage) {
+        this.bigImage = bigImage;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SensorManager sm = (SensorManager) (getActivity()).getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        //添加重力感应侦听，并实现其方法，
+        horizontalScrollView.setSmoothScrollingEnabled(true);
+
+        SensorEventListener sel = new SensorEventListener() {
+            public void onSensorChanged(final SensorEvent se) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        double x = se.values[SensorManager.DATA_X];
+                        double y = se.values[SensorManager.DATA_Y];
+                        double z = se.values[SensorManager.DATA_Z];
+                        CCLog.print("x=" + (int) x + "y=" + (int) y + "z=" + (int) z);
+                        ViewGroup.LayoutParams layoutParams=iv.getLayoutParams();
+//                        horizontalScrollView.setScrollIndicators();
+//                        horizontalScrollView.
+//                        if (horizontalScrollView.getsc)
+                        horizontalScrollView.smoothScrollBy((int) x,0);
+
+
+                    }
+                });
+
+
+            }
+
+            public void onAccuracyChanged(Sensor arg0, int arg1) {
+            }
+        };
+        //注册Listener，SENSOR_DELAY_GAME为检测的精确度，
+        sm.registerListener(sel, sensor, SensorManager.SENSOR_DELAY_GAME);
+    horizontalScrollView.setOnTouchListener(new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            return false;
+        }
+    });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+
+        if (bigImage.getPub_time() != null) {
+            tv0.setText(bigImage.getPub_time());
+            ViewGroup.LayoutParams parms = iv.getLayoutParams();
+            parms.height = ((BaseActivity) (getActivity())).getHight();
+            parms.width = (int) (((BaseActivity) (getActivity())).getHight() / StringUtil
+                    .toDouble(bigImage.getHeight(), 0) * StringUtil.toDouble
+
+                    (bigImage.getWidth(), 0));
+            iv.setLayoutParams(parms);
+            iv.requestLayout();
+
+            imageOptions = new ImageOptions.Builder()
+//                .setSize(((BaseActivity) getActivity()).getWidth(), ((BaseActivity) getActivity()
+                    .setIgnoreGif(false)
+                    .setImageScaleType(ImageView.ScaleType.CENTER_CROP)
+                    .setUseMemCache(true)
+                    .setFadeIn(true)
+                    .build();
+            x.image().bind(iv, bigImage.getOrigin_image_url(), imageOptions, new
+                    CustomBitmapLoadCallBack());
+        }
+    }
+
+    public class CustomBitmapLoadCallBack implements Callback.ProgressCallback<Drawable> {
+        AnimationDrawable mAnimate;
+
+        @Override
+        public void onWaiting() {
+            load.setVisibility(View.VISIBLE);
+            mAnimate = (AnimationDrawable) load.getBackground();
+            mAnimate.setOneShot(false);
+            mAnimate.start();
+        }
+
+        @Override
+        public void onStarted() {
+        }
+
+        @Override
+        public void onLoading(long total, long current, boolean isDownloading) {
+        }
+
+        @Override
+        public void onSuccess(Drawable result) {
+            mAnimate.stop();
+            load.setVisibility(View.GONE);
+
+        }
+
+        @Override
+        public void onError(Throwable ex, boolean isOnCallback) {
+        }
+
+        @Override
+        public void onCancelled(CancelledException cex) {
+        }
+
+        @Override
+        public void onFinished() {
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/henryzheng/ccimageshare/C/BigImageShow/BigImageShowFragment.java
@@ -0,0 +1,120 @@
+package com.example.henryzheng.ccimageshare.C.BigImageShow;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.view.View;
+
+import com.example.henryzheng.ccimageshare.C.Base.BaseFragment;
+import com.example.henryzheng.ccimageshare.C.mainfragments.i.MainFragmentInterface;
+import com.example.henryzheng.ccimageshare.C.mainfragments.model.ImageListBaseModel;
+import com.example.henryzheng.ccimageshare.C.mainfragments.p.MainFragmentsPresenter;
+import com.example.henryzheng.ccimageshare.M.ZuiMeiModel.Image;
+import com.example.henryzheng.ccimageshare.R;
+import com.example.henryzheng.ccimageshare.test.MyViewPage;
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ContentView(R.layout.fragment_big_image_show)
+public class BigImageShowFragment extends BaseFragment implements MainFragmentInterface {
+    private static int position=0;
+    @ViewInject(R.id.viewPage0)
+    private MyViewPage viewPager;
+    static List<Image> images = new ArrayList<>();
+    static int imageAmout = 1000;
+    static List<BigImageFragment> bigImageFragments;
+    private static MainFragmentsPresenter presenter;
+    static ImageListBaseModel imageListBaseModel;
+    static List<Image> existImages=new ArrayList<>();
+    
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    public static void setImagesAndModel(List<Image> loadImages, ImageListBaseModel
+            imageListBaseModel,int loadPosition) {
+        images = loadImages;
+        position=loadPosition;
+        initFragments();
+        BigImageShowFragment.imageListBaseModel = imageListBaseModel;
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        presenter=new MainFragmentsPresenter(this,imageListBaseModel.getUrl(),imageListBaseModel.getType());
+        presenter.setPage(images.size()/30);
+        if (bigImageFragments != null)
+            viewPager.setAdapter(new MyViewPageAdapt(getActivity().getSupportFragmentManager()));
+        viewPager.setCurrentItem(position);
+    }
+
+    private static void initFragments() {
+        bigImageFragments = new ArrayList<>();
+        for (int i =0; i < imageAmout; i++) {
+            BigImageFragment bigImageFragment = new BigImageFragment();
+            bigImageFragments.add(bigImageFragment);
+            if (i < images.size()) {
+                bigImageFragment.setBigImage(images.get(i));
+            }
+        }
+    }
+
+    /**
+     * 增加图片信息到fragments中
+     * @param addImages
+     */
+    private synchronized static void addImageToFragment(List<Image> addImages){
+        for (int i=0;i<30;i++){
+            bigImageFragments.get(images.size()+i).setBigImage(addImages.get(i));
+        }
+        images.addAll(addImages);
+    }
+
+
+    class MyViewPageAdapt extends FragmentStatePagerAdapter {
+
+        public MyViewPageAdapt(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if (position > images.size()) {
+                synchronized (presenter.getClass()){
+                    presenter.loadListData(presenter.LOAD_MORE_TYPE);
+
+                }
+            }
+            return bigImageFragments.get(position);
+
+        }
+
+        @Override
+        public int getCount() {
+            return bigImageFragments.size();
+        }
+    }
+
+    @Override
+    public void loadNewImages(List<Image> images) {
+        addImageToFragment(images);
+    }
+
+    @Override
+    public void refreshImages(List<Image> images) {
+
+    }
+
+}
